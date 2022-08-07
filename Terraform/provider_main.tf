ac373961c4402c2710e01c8aa1a5beab185532f3
Muralidar Sripadha <muralidar.sripadha@outlook.com>
2022-08-07 10:16:59 +0000

    Updated provider_main.tf

--- Terraform/provider_main.tf
+++ Terraform/provider_main.tf
@@ -11,9 +11,3 @@ terraform {
  provider "azurerm" {
    features {}
  } 
-  
-  subscription_id = var.azure-subscription-id
-  client_id       = var.azure-client-id
-  client_secret   = var.azure-client-secret
-  tenant_id       = var.azure-tenant-id
-}
