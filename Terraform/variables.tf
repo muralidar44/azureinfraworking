a91a41de4816b5e1e766248cf56d5c791c99b0c0
Muralidar Sripadha <muralidar.sripadha@outlook.com>
2022-08-07 12:39:58 +0000

    Updated variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -26,3 +26,37 @@ variable "linux_admin_username" {
   type        = string
   description = "linux username"  
 }
+variable "mediavnetcidr" {
+type = string
+description = "This is the mediawiki vnet cidr"
+}
+
+variable "mediavnet" {
+type = string
+description = "This is the mediawiki vnet"
+}
+
+variable "mediawebsubnetcidr" {
+type = string
+description = "This is the mediawiki mediawebsubnet"
+}
+
+variable "mediawebsubnet" {
+type = string
+description = "This is the mediawiki mediawebsubnet"
+}
+
+variable "lbpublicipname" {
+type = string
+description = "This is the Load balancer public Ip Name"
+}
+
+variable "medialbname" {
+type = string
+description = "This is the Load balancer Name"
+}
+
+variable "lbbackendpoolname" {
+type = string
+description = "This is the Load balancer backend pool name"
+}
