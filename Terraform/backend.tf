2a952aa5a7219bf78de8684a6fb1f3c69fee58e2
Muralidar Sripadha <muralidar.sripadha@outlook.com>
2022-08-08 12:32:36 +0000

    Updated backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -6,7 +6,9 @@
     resource_group_name  = "tstate-rg"
     storage_account_name = "tfstr8811"
     container_name       = "tfstate"
-    key                  = "W0q2+9GdV0Dh9Hjy3uGIK49RWnhYy9D3/kpvEtPD+ENt9ghTQqWz07zwDyXKy4HGkc+D1EYH3iAk+AStfV6zGw=="
+    key                 = "mediawiki.tfstate"
+    access_key           = "W0q2+9GdV0Dh9Hjy3uGIK49RWnhYy9D3/kpvEtPD+ENt9ghTQqWz07zwDyXKy4HGkc+D1EYH3iAk+AStfV6zGw=="
+
   }
 }
 # Configure the Azure provider
