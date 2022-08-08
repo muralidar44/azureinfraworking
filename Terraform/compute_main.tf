4fc2e52106b214e9b9c497feb81d3839360aa9ff
Muralidar Sripadha <muralidar.sripadha@outlook.com>
2022-08-08 08:19:34 +0000

    Updated compute_main.tf

--- Terraform/compute_main.tf
+++ Terraform/compute_main.tf
@@ -13,15 +13,51 @@ resource "random_password" "linux-vm-password" {
   override_special = "!@#$%&"
 }
 
-resource "azurerm_availability_set" "mediaavsetdb" {
-   name                         = var.mediaavsetname
-   location                     = azurerm_resource_group.mediarg.location
-   resource_group_name          = azurerm_resource_group.mediarg.name
-   platform_fault_domain_count  = 2
-   platform_update_domain_count = 2
-   managed                      = true
- }
 
+ resource "azurerm_virtual_machine" "webapp" {
+   count                 = 2
+   name                  = "webapp${count.index}"
+   location              = azurerm_resource_group.mediarg.location
+   availability_set_id   = azurerm_availability_set.mediaavset.id
+   resource_group_name   = azurerm_resource_group.mediarg.name
+   network_interface_ids = [element(azurerm_network_interface.webappnics.*.id, count.index)]
+   vm_size               = "Standard_DS1_v2"
+
+   # Uncomment this line to delete the OS disk automatically when deleting the VM
+   delete_os_disk_on_termination = true
+
+   # Uncomment this line to delete the data disks automatically when deleting the VM
+   # delete_data_disks_on_termination = true
+
+   storage_image_reference {
+     publisher = "RedHat"
+     offer     = "RHEL"
+     sku       = "8.2"
+     version   = "latest"
+   }
+
+   storage_os_disk {
+     name              = "myosdisk${count.index}"
+     caching           = "ReadWrite"
+     create_option     = "FromImage"
+     managed_disk_type = "Standard_LRS"
+   }
+
+   
+   os_profile {
+     computer_name  = "hostname"
+     admin_username = "testadmin"
+     admin_password = "Password1234!"
+   }
+
+   os_profile_linux_config {
+     disable_password_authentication = false
+   }
+
+   tags = {
+     environment = "staging"
+   }
+ }
 resource "azurerm_virtual_machine_scale_set" "appvmss" {
   name                = "appwebvmss"
   location            = azurerm_resource_group.mediarg.location
@@ -66,7 +102,18 @@ resource "azurerm_virtual_machine_scale_set" "appvmss" {
    os_profile_linux_config {
      disable_password_authentication = false
    }
+network_profile {
+    name    = "appvmnetworkprofile"
+    primary = true
 
+    ip_configuration {
+      name                                   = "appvmconfig"
+      primary                                = true
+      subnet_id                              = azurerm_subnet.appvmsubnet.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.lbappbp.id]
+      load_balancer_inbound_nat_rules_ids    = [azurerm_lb_nat_pool.lbnatpool.id]
+    }
+  }
 
    tags = {
      environment = "staging"
