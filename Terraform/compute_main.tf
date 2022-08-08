3f83764ce0b6287694e8dc024c1f53c01f01de06
Muralidar Sripadha <muralidar.sripadha@outlook.com>
2022-08-08 09:36:48 +0000

    Updated compute_main.tf

--- Terraform/compute_main.tf
+++ Terraform/compute_main.tf
@@ -26,6 +26,30 @@ resource "azurerm_virtual_machine" "dbvm" {
 
    # Uncomment this line to delete the data disks automatically when deleting the VM
    # delete_data_disks_on_termination = true
+   storage_image_reference {
+     publisher = "RedHat"
+     offer     = "RHEL"
+     sku       = "8.2"
+     version   = "latest"
+   }
+
+   storage_os_disk {
+     name              = "myosdisk"
+     caching           = "ReadWrite"
+     create_option     = "FromImage"
+     managed_disk_type = "Standard_LRS"
+   }
+
+   
+   os_profile {
+     computer_name  = "hostname"
+     admin_username = "testadmin"
+     admin_password = "Password1234!"
+   }
+
+   os_profile_linux_config {
+     disable_password_authentication = false
+   }
 }
  resource "azurerm_virtual_machine_scale_set" "appvmss" {
   name                = "appwebvmss"
@@ -44,7 +68,7 @@ resource "azurerm_virtual_machine" "dbvm" {
   }
 
   # required when using rolling upgrade policy
-  health_probe_id = azurerm_lb_probe.example.id
+  health_probe_id = azurerm_lb_probe.lbprobe.id
 
    storage_profile_image_reference {
      publisher = var.webvm_image_publisher
