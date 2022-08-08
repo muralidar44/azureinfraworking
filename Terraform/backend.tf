c294816a7314306f36228ca484ac35f79a30b7c3
Muralidar Sripadha <muralidar.sripadha@outlook.com>
2022-08-08 12:52:04 +0000

    Updated backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,4 +1,10 @@
-# Define Terraform provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
 backend "azurerm" {
     resource_group_name  = "tstate-rg"
     storage_account_name = "tfstr8811"
@@ -7,3 +13,4 @@ backend "azurerm" {
     access_key           = "W0q2+9GdV0Dh9Hjy3uGIK49RWnhYy9D3/kpvEtPD+ENt9ghTQqWz07zwDyXKy4HGkc+D1EYH3iAk+AStfV6zGw=="
 
   }
+}
