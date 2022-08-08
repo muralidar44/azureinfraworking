89d07315fb3ab189618944ea0138c651c6c8bf7d
Muralidar Sripadha <muralidar.sripadha@outlook.com>
2022-08-08 12:43:36 +0000

    Updated backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,8 +1,5 @@
 # Define Terraform provider
-
- terraform {
-   required_version = ">=0.12"
-  backend "azurerm" {
+backend "azurerm" {
     resource_group_name  = "tstate-rg"
     storage_account_name = "tfstr8811"
     container_name       = "tfstate"
@@ -10,8 +7,3 @@
     access_key           = "W0q2+9GdV0Dh9Hjy3uGIK49RWnhYy9D3/kpvEtPD+ENt9ghTQqWz07zwDyXKy4HGkc+D1EYH3iAk+AStfV6zGw=="
 
   }
-}
-# Configure the Azure provider
- provider "azurerm" { 
-     features {}
-}
