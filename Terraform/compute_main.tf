85e3e42d042aeaef91438bfd274158c1e21774dd
Muralidar Sripadha <muralidar.sripadha@outlook.com>
2022-08-08 12:26:30 +0000

    Updated compute_main.tf

--- Terraform/compute_main.tf
+++ Terraform/compute_main.tf
@@ -19,7 +19,7 @@ resource "azurerm_virtual_machine" "dbvm" {
    location              = azurerm_resource_group.mediarg.location   
    resource_group_name   = azurerm_resource_group.mediarg.name
    network_interface_ids = [element(azurerm_network_interface.dbvmnic.*.id, count.index)]
-   vm_size               = "Standard_DS1_v2"
+   vm_size               = "Standard_D4s_v3"
 
    # Uncomment this line to delete the OS disk automatically when deleting the VM
    delete_os_disk_on_termination = true
@@ -70,7 +70,7 @@ resource "azurerm_virtual_machine" "dbvm" {
   # required when using rolling upgrade policy
   health_probe_id = azurerm_lb_probe.lbprobe.id
     sku {
-    name     = "Standard_F2"
+    name     = "Standard_D4s_v3"
     tier     = "Standard"
     capacity = 2
   }
