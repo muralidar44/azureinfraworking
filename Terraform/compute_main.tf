fa0627bd6a42bbf61f9a683eebfe0d41a7ab1390
Muralidar Sripadha <muralidar.sripadha@outlook.com>
2022-08-08 09:24:47 +0000

    Updated compute_main.tf

--- Terraform/compute_main.tf
+++ Terraform/compute_main.tf
@@ -14,21 +14,7 @@ resource "random_password" "linux-vm-password" {
 }
 
 
- resource "azurerm_virtual_machine" "dbvm" {
-   count                 = 1
-   name                  = "dbvm${count.index}"
-   location              = azurerm_resource_group.mediarg.location
-   resource_group_name   = azurerm_resource_group.mediarg.name
-   network_interface_ids = [element(azurerm_network_interface.dbvmnic.*.id, count.index)]
-   vm_size               = "Standard_DS1_v2"
-
-   # Uncomment this line to delete the OS disk automatically when deleting the VM
-   delete_os_disk_on_termination = true
-
-   # Uncomment this line to delete the data disks automatically when deleting the VM
-   # delete_data_disks_on_termination = true
- }
-resource "azurerm_virtual_machine_scale_set" "appvmss" {
+ resource "azurerm_virtual_machine_scale_set" "appvmss" {
   name                = "appwebvmss"
   location            = azurerm_resource_group.mediarg.location
   resource_group_name = azurerm_resource_group.mediarg.name
